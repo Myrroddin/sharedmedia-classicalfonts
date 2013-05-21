73065437082cb9ed1e84890cd29043470403d604
myrroddin <myrroddin@eebd27d4-17b2-4276-881c-e4a9e8adc4e8>
2013-05-21 12:42:54 +0000

    - First public alpha

---
+++ trunk/Main.lua
@@ -0,0 +1,12 @@
+local lsm = LibStub("LibSharedMedia-3.0" true)
+
+lsm:Register("font", "Augustus", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\AUGUSTUS")
+lsm:Register("font", "Caesar", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\CAESAR")
+lsm:Register("font", "Capitalis Type Oasis", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\CapitalisTypOasis")
+lsm:Register("font", "Diogenes", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\DIOGENES")
+lsm:Register("font", "Capitalis Type Oasis", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\CapitalisTypOasis")
+lsm:Register("font", "Ancient Geek", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\geek")
+lsm:Register("font", "Marathon", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\marav2")
+lsm:Register("font", "Marathon 2", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\mara2v2")
+lsm:Register("font", "Roman", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\Roman SD")
+lsm:Register("font", "Triathalon", "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\Triatlhon In")
