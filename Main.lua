e1401da654c8f3f1c540f47d66ae84f1b9a44492
Myrroddin <psvander@gmail.com>
2026-06-03 12:00:56 -0600

    [Mists] Bump ToC for patch 5.5.4

--- Main.lua
+++ Main.lua
@@ -1,29 +1,38 @@
+---@class LibSharedMediaMediaTypes
+---@field FONT string
+
+---@class LibSharedMedia
+---@field MediaType LibSharedMediaMediaTypes
+---@field Register fun(self: LibSharedMedia, mediaType: string, key: string, data: string): boolean
+
+---@type LibSharedMedia
 local LSM = LibStub("LibSharedMedia-3.0")
+
 local MediaType_FONT = LSM.MediaType.FONT or "font"
 
--- Define font list
+---@type table<string, string>
 local fonts = {
-	["Augustus"]				= "AUGUSTUS.ttf",
-	["Augustus Beveled"]		= "Augustus Beveled.ttf",
-	["Caesar"]					= "CAESAR.ttf",
-	["Diogenes"]				= "DIOGENES.ttf",
-	["Capitalis Type Oasis"]	= "CapitalisTypOasis.ttf",
-	["Ancient Geek"]			= "geek.ttf",
-	["Marathon"]				= "mara2v2.ttf",
-	["Roman SD"]				= "Roman SD.ttf",
-	["Triatlhon In"]			= "Triatlhon In.ttf",
-	["ROMANUM EST ALL CAPS"]	= "Romanum Est.ttf",
-	["King Arthur Legend"]		= "King Arthur Legend.ttf",
-	["Olde English"]			= "OldeEnglish.ttf",
-	["Germanica"]				= "Plain Germanica.ttf",
-	["Germanica Shadowed"]		= "Shadowed Germanica.ttf",
-	["Germanica Embossed"]		= "Embossed Germanica.ttf",
-	["Germanica Fluted"]		= "Fluted Germanica.ttf",
-	["Cleopatra"]				= "Cleopatra.ttf",
+	["Augustus"] = "AUGUSTUS.ttf",
+	["Augustus Beveled"] = "Augustus Beveled.ttf",
+	["Caesar"] = "CAESAR.ttf",
+	["Diogenes"] = "DIOGENES.ttf",
+	["Capitalis Type Oasis"] = "CapitalisTypOasis.ttf",
+	["Ancient Geek"] = "geek.ttf",
+	["Marathon"] = "mara2v2.ttf",
+	["Roman SD"] = "Roman SD.ttf",
+	["Triatlhon In"] = "Triatlhon In.ttf",
+	["ROMANUM EST ALL CAPS"] = "Romanum Est.ttf",
+	["King Arthur Legend"] = "King Arthur Legend.ttf",
+	["Olde English"] = "OldeEnglish.ttf",
+	["Germanica"] = "Plain Germanica.ttf",
+	["Germanica Shadowed"] = "Shadowed Germanica.ttf",
+	["Germanica Embossed"] = "Embossed Germanica.ttf",
+	["Germanica Fluted"] = "Fluted Germanica.ttf",
+	["Cleopatra"] = "Cleopatra.ttf",
 }
 
--- Register fonts
 local basePath = "Interface\\AddOns\\SharedMedia_ClassicalFonts\\Fonts\\"
-for name, file in pairs(fonts) do
-	LSM:Register(MediaType_FONT, name, basePath .. file)
+
+for name, fileName in pairs(fonts) do
+	LSM:Register(MediaType_FONT, name, basePath .. fileName)
 end
